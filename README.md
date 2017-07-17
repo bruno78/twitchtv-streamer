a0981a7da8041df1503f439faad8ab943818c586
bruno78 <devbruno78@gmail.com>
2017-07-17 02:27:41 -0400

    Finish API Scripting

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 An application to keep track of some Twitch.TV streamers.
 It uses Twitch.TV JSON API
+
+To do:
+
+-[ ] Add tabs
+-[ ] Add search (optional)
+-[ ] Finish styling
--- twitchtv.js
+++ twitchtv.js
@@ -1,29 +1,62 @@
-//  To get a client ID, request should be
-// curl -H Accept: application/vnd.twitchtv.v5+json
-// -H Client-ID: uo6dggojyb8d6soh92zknwmi5ej1q2'
-// -X GET https://api.twitch.tv/kraken/users?login=dallas,dallasnchains
-const response = {
-   "_total": 2,
-   "users": [
-      {
-         "_id": "44322889",
-         "bio": "Just a gamer playing games and chatting. :)",
-         "created_at": "2013-06-03T19:12:02.580593Z",
-         "display_name": "dallas",
-         "logo": "https://static-cdn.jtvnw.net/jtv_user_pictures/dallas-profile_image-1a2c906ee2c35f12-300x300.png",
-         "name": "dallas",
-         "type": "staff",
-         "updated_at": "2017-02-09T16:32:06.784398Z"
-      },
-      {
-         "_id": "129454141",
-         "bio": null,
-         "created_at": "2016-07-13T14:40:42.398257Z",
-         "display_name": "dallasnchains",
-         "logo": null,
-         "name": "dallasnchains",
-         "type": "user",
-         "updated_at": "2017-02-04T14:32:38.626459Z"
-      }
-   ]
+const streamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
+let twitchAPIURL = "https://wind-bow.glitch.me/twitch-api"
+
+let liGenerator = (user, logo, infoStatus, status)=> {
+  return $("<li/>", {
+              class:"collection-item avatar valign-wrapper " + status,
+              html: '<img src="' + logo + '" alt="avatar image" class="circle">' +
+                    '<a href="https://www.twitch.tv/' + user + '" id="user" class="title center-align" target="_blank">' + user + '</a>' +
+                    '<span id="info" class="description truncate center-align">' + infoStatus + '<span/>'
+          })
 }
+
+let search = ()=> {
+  let query = $('input').val().toLowerCase();
+  $('.collection li').each(()=> {
+    let name = $(this).find('#user').text().toLowerCase();
+    if (name.indexOf(query) === -1) {
+      $(this).addClass('filtered')
+    }
+    else {
+      $(this).removeClass('filtered')
+    }
+  })
+}
+
+
+$(document).ready(()=>{
+  let listOfResults = []
+
+  streamers.forEach((streamer)=>{
+    let logo
+    let infoStatus
+    let status
+    let usersLink = twitchAPIURL + "/users/" + streamer + "?callback=?"
+    let streamLink = twitchAPIURL + "/streams/" + streamer + "?callback=?"
+
+    $.getJSON(usersLink, (data)=>{
+      logo = data.logo !== null ? data.logo : "http://placekitten.com/g/50/50"
+    })
+
+    $.getJSON(streamLink, (data)=>{
+      if (data.stream === null) {
+        infoStatus = "<em>Offline</em>"
+        status = "offline"
+      }
+      else {
+        status = "online"
+      }
+      let channelLink = twitchAPIURL + "/channels/" + streamer + "?callback=?"
+      $.getJSON(channelLink, (data)=>{
+        if(infoStatus !== "<em>Offline</em>") {
+          infoStatus = data.status
+        }
+        $(".collection").append(liGenerator(streamer, logo, infoStatus, status))
+      })
+    })
+  }) // .forEach
+
+  $('ul.tabs').tabs()
+  $('input').keyup(search())
+
+})
